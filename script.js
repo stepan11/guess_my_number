398bffd0f95d8fe3b0d6754e1de5184fcadf60f2
stepan11 <stepanuch953@gmail.com>
2021-05-23 18:06:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -84,7 +84,7 @@ document.querySelector('.btn').addEventListener('click', function () {
   score = 20;
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   // document.querySelector('.message').textContent = 'Start guessing...';
-  displayMessage(Start guessing...🙄)
+  displayMessage('Start guessing...🙄')
   document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
